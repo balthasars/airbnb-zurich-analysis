372c67d3f823c7213c2f40636351e9ae8d8dcd16
Balthasar <balthasar.sager@gmail.com>
2022-05-27 15:52:11 +0200

    add detailed listings and start making map

--- scr/01-download-data.py
+++ scr/01-download-data.py
@@ -2,7 +2,12 @@
 import pandas as pd
 
 # get data from Inside Airbnb and save as CSV
-url = "http://data.insideairbnb.com/switzerland/z%C3%BCrich/zurich/2021-12-28/visualisations/listings.csv"
+url_1 = "http://data.insideairbnb.com/switzerland/z%C3%BCrich/zurich/2022-03-29/visualisations/listings.csv"
 
 # read data from url and save as CSV
-pd.read_csv(url).to_csv("data/listings.csv", index=False)
+pd.read_csv(url_1).to_csv("data/listings.csv", index=False)
+
+# add additional info
+
+url_2 = "http://data.insideairbnb.com/switzerland/z%C3%BCrich/zurich/2022-03-29/data/listings.csv.gz"
+pd.read_csv(url_2, compression='gzip').to_csv("data/listings_detailed.csv", index=False)
